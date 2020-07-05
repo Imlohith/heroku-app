1f503e21bd8f0606d299eaafa0efe763f8c2238f
lohith <viperlohith121@gmail.com>
2020-07-05 11:27:13 +0530

    added port

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 
 const app = express();
 
+var port = process.env.PORT || 8080;
+
+
 app.use("/", (req, res) => {
     res.send("hello from heroku-app")
 })
 
-app.listen(3000, () => {
-    console.log('app liestimg at 3000 port')
+app.listen(port, () => {
+    console.log('app started')
 })
